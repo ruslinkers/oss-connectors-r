6326ef8a38a29bf46c5d69cd831370b7f664e2c0
Oleg Belyaev <obelyaev@gmail.com>
2025-04-09 23:16:23 +0300

    update interface according to new version

--- shared.R
+++ shared.R
@@ -7,24 +7,24 @@ knitr::opts_chunk$set(echo=FALSE, message=FALSE)
 
 # Load data
 # read.csv("nov23-all.csv",stringsAsFactors = FALSE) -> linkers
-read.xlsx("ossetic_linkers_syntax.xlsx", na.strings=c("NA","")) -> linkers
+read.xlsx("ossetic_linkers_syntax_rev_2_0.xlsx", na.strings=c("NA","")) -> linkers
 
 normalize_factor <- function(f) {
   out <- str_to_lower(f)
-  out <- sub(":","",out)
-  out <- gsub(" +"," ",out)
+  out <- sub(":", "", out)
+  out <- gsub(" +", " ", out)
   return(out)
 }
 
 normalize_clause.pos <- function(f) {
-  out <- sub(" зависимой клаузы","",f)
-  out <- sub("постпозиция и препозиция","препозиция и постпозиция",out)
-  out <- sub("свободный","свободная",out)
+  out <- sub(" зависимой клаузы", "", f)
+  out <- sub("постпозиция и препозиция","препозиция и постпозиция", out)
+  out <- sub("свободный", "свободная", out)
   return(out)
 }
 
 linebreaks <- function(x) {
-  return(gsub("&#10;","<br>",x))
+  return(gsub("&#10;", "<br>", x))
 }
 
 # Change POS, semantic fields, classes to factors
@@ -32,95 +32,80 @@ linkers %>%
   mutate(
     across(all_of(names(.)), trimws)
   ) %>%
-  separate_wider_delim('Позиция.коррелята', 
-                       delim="&#10;", 
-                       names=c('correl.pos','correl.pos.ex'),
-                       too_few='align_start',
-                       too_many='merge') %>%
-  separate_wider_delim('Опущение.коррелята', 
-                       delim="&#10;", 
-                       names=c('correl.omit','correl.omit.ex'),
-                       too_few='align_start',
-                       too_many='merge') %>%
-  separate_wider_delim('Позиция.клаузы.с.коннектором', 
-                       delim="&#10;", 
-                       names=c('clause.pos','clause.pos.ex'),
-                       too_few='align_start',
-                       too_many='merge') %>%
-  separate_wider_delim('Позиция.коннектора.в.клаузе', 
-                       delim="&#10;", 
-                       names=c('conn.pos','conn.pos.ex'),
-                       too_few='align_start',
-                       too_many='merge') %>%
-  separate_wider_delim('Модификация.коррелята', 
-                       delim="&#10;", 
-                       names=c('correl.mod','correl.mod.ex'),
-                       too_few='align_start',
-                       too_many='merge') %>%
-  separate_wider_delim('Фокусирование', 
-                       delim="&#10;", 
-                       names=c('focus','focus.ex'),
-                       too_few='align_start',
-                       too_many='merge') %>%
-  separate_wider_delim('Независимое.употребление.клаузы.с.коннектором', 
-                       delim="&#10;", 
-                       names=c('clause.indep','clause.indep.ex'),
-                       too_few='align_start',
-                       too_many='merge') %>%
-  separate_wider_delim('Иллокутивное.употребление', 
-                       delim="&#10;", 
-                       names=c('illoc','illoc.ex'),
-                       too_few='align_start',
-                       too_many='merge') %>%
   mutate(
-    # id = as.numeric(id),
     marker = as.character(marker),
-    semfield = as.factor(normalize_factor(sem.field)),    
+    semfield = as.factor(normalize_factor(sem.field)),
     gloss.ru = as.character(gloss.ru),
     corr = as.character(corr),
     example = as.character(example),
     tr.ru = as.character(tr.ru),
-    clause.pos = as.factor(normalize_clause.pos(normalize_factor(clause.pos))),
-    clause.pos.ex = as.character(linebreaks(clause.pos.ex)),
-    conn.pos = as.factor(normalize_factor(conn.pos)),    
-    conn.pos.ex = as.character(linebreaks(conn.pos.ex)),
-    correl.pos = as.factor(normalize_factor(correl.pos)),
-    correl.pos.ex = as.character(linebreaks(correl.pos.ex)),
-    correl.omit = as.factor(normalize_factor(correl.omit)),
-    correl.omit.ex = as.character(linebreaks(correl.omit.ex)),
-    correl.mod = as.factor(normalize_factor(correl.mod)),
-    correl.mod.ex = as.character(linebreaks(correl.mod.ex)),
-    focus = as.factor(normalize_factor(focus)),
-    focus.ex = as.character(linebreaks(focus.ex)),
-    clause.indep = as.factor(normalize_factor(clause.indep)),
-    clause.indep.ex = as.character(linebreaks(clause.indep.ex)),
-    illoc = as.factor(normalize_factor(illoc)),
-    illoc.ex = as.character(linebreaks(illoc.ex)),
+
+    clause.pos = as.factor(normalize_factor(clause.position)),
+    clause.pos.ex = as.character(linebreaks(clause.position.ex)),
+    clause.pos.comm = as.character(linebreaks(clause.position.comm)),
+
+    conn.pos = as.factor(normalize_factor(linker.position)),
+    conn.pos.ex = as.character(linebreaks(linker.position.ex)),
+    conn.pos.comm = as.character(linebreaks(linker.position.comm)),
+
+    correl.pos = as.factor(normalize_factor(corr.position)),
+    correl.pos.ex = as.character(linebreaks(corr.position.ex)),
+    correl.pos.comm = as.character(linebreaks(corr.position.comm)),
+
+    correl.omit = as.factor(normalize_factor(no.corr)),
+    correl.omit.ex = as.character(linebreaks(no.corr.ex)),
+    correl.omit.comm = as.character(linebreaks(no.corr.comm)),
+
+    correl.mod = as.factor(normalize_factor(corr.modification)),
+    correl.mod.ex = as.character(linebreaks(corr.modification.ex)),
+
+    focus = as.factor(normalize_factor(focusing)),
+    focus.ex = as.character(linebreaks(focusing.ex)),
+
+    clause.indep = as.factor(normalize_factor(independent.use)),
+    clause.indep.ex = as.character(linebreaks(independent.use.ex)),
+    clause.indep.comm = as.character(linebreaks(independent.use.comm)),
+
+    illoc = as.factor(normalize_factor(speech.act.use)),
+    illoc.ex = as.character(linebreaks(speech.act.use.ex)),
     .keep = 'unused',
   ) %>%
   select(marker,
-         semfield,
-         gloss.ru,
-         corr,
-         example,
-         tr.ru,
-         clause.pos,
-         clause.pos.ex,
-         conn.pos,
-         conn.pos.ex,
-         correl.pos,
-         correl.pos.ex,
-         correl.omit,
-         correl.omit.ex,
-         correl.mod,
-         correl.mod.ex,
-         focus,
-         focus.ex,
-         clause.indep,
-         clause.indep.ex,
-         illoc,
-         illoc.ex
-    ) -> linkers
+    semfield,
+    gloss.ru,
+    corr,
+    example,
+    tr.ru,
+
+    clause.pos,
+    clause.pos.ex,
+    clause.pos.comm,
+
+    conn.pos,
+    conn.pos.ex,
+    conn.pos.comm,
+
+    correl.pos,
+    correl.pos.ex,
+    correl.pos.comm,
+
+    correl.omit,
+    correl.omit.ex,
+    correl.omit.comm,
+
+    correl.mod,
+    correl.mod.ex,
+
+    focus,
+    focus.ex,
+
+    clause.indep,
+    clause.indep.ex,
+    clause.indep.comm,
+
+    illoc,
+    illoc.ex
+  ) -> linkers
 
 custom_aggr <- "
     function(values, rows) {
